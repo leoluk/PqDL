fa5bde75e233a9e1f885923730b476100aee0753
Leopold Schabel <mail@leoschabel.de>
2010-06-06 11:28:58 +0000

    v0.2.0

--- pqdl.py
+++ pqdl.py
@@ -20,7 +20,7 @@ need to do it by hand or with this script.
 This script is written by leoluk. Please look at www.leoluk.de/paperless-caching/pqdl
 """
 
-version = "0.2.0-trunk"
+version = "0.2.0-stable"
 
 import mechanize
 import optparse
